f36a7406827dced73c058de32393ec8ec0487285
Jorge Costa <jorge.costa@trimble.com>
2024-04-12 11:16:32 +0300

    upgrade to .net 8.0

--- PublishAndPackage.bat
+++ PublishAndPackage.bat
@@ -6,9 +6,9 @@ dotnet publish -c Release -r osx-x64 --self-contained true
 copy /Y Tools\CxxSonarQubeRunnerLinux.sh BuildDrop\Publish
 copy /Y Tools\CxxSonarQubeRunnerOsx.sh BuildDrop\Publish
 copy /Y Tools\CxxSonarQubeRunnerWin.bat BuildDrop\Publish
-robocopy /MIR BuildDrop\Runner\net6.0\linux-x64\publish BuildDrop\Publish\linux-x64
-robocopy /MIR BuildDrop\Runner\net6.0\osx-x64\publish BuildDrop\Publish\osx-x64
-robocopy /MIR BuildDrop\Runner\net6.0\win-x64\publish BuildDrop\Publish\win-x64
+robocopy /MIR BuildDrop\Runner\net8.0\linux-x64\publish BuildDrop\Publish\linux-x64
+robocopy /MIR BuildDrop\Runner\net8.0\osx-x64\publish BuildDrop\Publish\osx-x64
+robocopy /MIR BuildDrop\Runner\net8.0\win-x64\publish BuildDrop\Publish\win-x64
 cd BuildDrop\Publish
 "C:\Program Files\7-Zip\7z.exe" a -tzip CxxSonarQubeRunner.zip .
 move CxxSonarQubeRunner.zip ..\..
